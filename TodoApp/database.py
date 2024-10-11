484eb95f513722933e7f9e91a073a551db7f1042
dimwarlock <screanvir@gmail.com>
2024-10-11 12:01:16 -0300

    Refactor with sqlite3 database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:@localhost:3306/todoapplicationdatabase'
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:@localhost:3306/todoapplicationdatabase'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
